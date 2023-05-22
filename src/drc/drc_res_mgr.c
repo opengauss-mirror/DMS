648da4a46b40d56dab4332b03219b208f5b8bb4d
andy_git <andymind@126.com>
2023-05-22 11:32:10 +0800

    fix alloc res fail

--- src/drc/drc_res_mgr.c
+++ src/drc/drc_res_mgr.c
@@ -27,7 +27,7 @@
 #include "securec.h"
 #include "dms_error.h"
 #include "dcs_page.h"
-
+#include "drc_page.h"
 /* some global struct definition */
 drc_res_ctx_t g_drc_res_ctx;
 
@@ -193,7 +193,6 @@ char* drc_res_pool_alloc_item(drc_res_pool_t* pool)
     if (cm_bilist_empty(&pool->free_list)) {
         item_addr = drc_res_pool_try_extend_and_alloc(pool);
         cm_spin_unlock(&pool->lock);
-        cm_panic(item_addr != NULL);
         return item_addr;
     }
     item_addr = (char *)cm_bilist_pop_first(&pool->free_list);
@@ -293,15 +292,45 @@ static void init_buf_res(drc_buf_res_t* buf_res, char* resid, uint16 len, uint8 
     DMS_SECUREC_CHECK(ret);
 }
 
+static uint32 drc_recycle_buf_res_directly(char *resid)
+{
+    drc_res_ctx_t *ctx = DRC_RES_CTX;
+    bilist_t *part_list = NULL;
+    uint16 part_id = drc_page_partid(resid);
+    uint16 part_cnt = 0;
+    do {
+        part_list = &ctx->global_buf_res.res_parts[part_id];
+        cm_spin_lock(&ctx->global_buf_res.res_parts_lock[part_id], NULL);
+        uint32 recycled = drc_recycle_buf_res_by_part(part_list, DRC_RES_PAGE_TYPE, 1, CM_FALSE);
+        cm_spin_unlock(&ctx->global_buf_res.res_parts_lock[part_id]);
+        if (recycled != 0) {
+            return recycled;
+        }
+        part_id++;
+        part_id = part_id % DRC_MAX_PART_NUM;
+        part_cnt++;
+    } while (part_cnt < DRC_MAX_PART_NUM);
+    return 0;
+}
+
 static drc_buf_res_t* drc_create_buf_res(drc_res_pool_t *pool, char *resid, uint16 len, uint8 res_type,
     drc_res_bucket_t *bucket)
 {
-    drc_buf_res_t* buf_res = (drc_buf_res_t *)drc_res_pool_alloc_item(pool);
-    if (buf_res == NULL) {
-        DMS_THROW_ERROR(ERRNO_DMS_DRC_PAGE_POOL_CAPACITY_NOT_ENOUGH);
-        return NULL;
-    }
-    LOG_DEBUG_INF("[DRC][%s]buf_res create", cm_display_pageid(resid));
+    drc_buf_res_t* buf_res;
+    do {
+        buf_res = (drc_buf_res_t *)drc_res_pool_alloc_item(pool);
+        if (buf_res != NULL) {
+            break;
+        }
+        LOG_DEBUG_WAR("[DRC][%s]buf_res create fail", cm_display_pageid(resid));
+        uint32 recycled = drc_recycle_buf_res_directly(resid);
+        if (recycled == 0) {
+                LOG_DEBUG_WAR("[DRC][%s]buf_res recycle fail", cm_display_pageid(resid));
+                DMS_DRC_SHORT_SLEEP;
+        }
+    } while (CM_TRUE);
+    LOG_DEBUG_INF("[DRC][%s]buf_res create successful", cm_display_pageid(resid));
+
     init_buf_res(buf_res, resid, len, res_type);
     drc_res_map_add_res(bucket, (char *)buf_res);
     if (res_type == DRC_RES_PAGE_TYPE) {
