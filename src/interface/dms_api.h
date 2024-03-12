dc9614e308624feaedc5355758a49420a4c39481
dongning12 <dning2017@163.com>
2024-03-12 10:47:53 +0800

    【openGauss】buf_ctrl增加ctrl_lock用于在DB侧对buf_ctrl进行控制

--- src/interface/dms_api.h
+++ src/interface/dms_api.h
@@ -33,7 +33,7 @@ extern "C" {
 #define DMS_LOCAL_MINOR_VER_WEIGHT  1000
 #define DMS_LOCAL_MAJOR_VERSION     0
 #define DMS_LOCAL_MINOR_VERSION     0
-#define DMS_LOCAL_VERSION           134
+#define DMS_LOCAL_VERSION           135
 
 #define DMS_SUCCESS 0
 #define DMS_ERROR (-1)
@@ -437,6 +437,7 @@ typedef struct st_dms_buf_ctrl {
     unsigned long long  pblk_lsn;
     unsigned char seg_fileno;
     unsigned int seg_blockno;
+    void* ctrl_lock;
 #endif
 } dms_buf_ctrl_t;
 
